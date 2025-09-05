3f7282ef0813f2fc0d8ee1fe31984a8fe6a12941
szolzol <51864754+szolzol@users.noreply.github.com>
2025-09-05 23:12:38 +0000

    Edited Spark

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,72 +2,71 @@ import { createContext, useContext, useEffect, useState } from "react"
 
 type Theme = "dark" | "light" | "system"
 
-type ThemeProviderProps = {
-  children: React.ReactNode
-  defaultTheme?: Theme
-  storageKey?: string
 }
-
 type ThemeProviderState = {
-  theme: Theme
-  setTheme: (theme: Theme) => void
-}
+  setTheme: (theme: Th
 
-const initialState: ThemeProviderState = {
-  theme: "system",
-  setTheme: () => null,
-}
+ 
 
-const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
-
-export function ThemeProvider({
-  children,
+const ThemeProviderContext 
+export functio
   defaultTheme = "system",
-  storageKey = "vite-ui-theme",
-  ...props
-}: ThemeProviderProps) {
-  const [theme, setTheme] = useState<Theme>(
-    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
+ 
+
   )
+  useEffect(() => 
 
-  useEffect(() => {
-    const root = window.document.documentElement
 
-    root.classList.remove("light", "dark")
 
-    if (theme === "system") {
-      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-        .matches
         ? "dark"
-        : "light"
 
-      root.classList.add(systemTheme)
       return
-    }
 
-    root.classList.add(theme)
   }, [theme])
-
   const value = {
-    theme,
-    setTheme: (theme: Theme) => {
-      localStorage.setItem(storageKey, theme)
+    setThe
       setTheme(theme)
-    },
   }
-
   return (
-    <ThemeProviderContext.Provider {...props} value={value}>
-      {children}
-    </ThemeProviderContext.Provider>
-  )
-}
+   
 
-export const useTheme = () => {
-  const context = useContext(ThemeProviderContext)
 
-  if (context === undefined)
-    throw new Error("useTheme must be used within a ThemeProvider")
+  const context = useContext(ThemeProviderContex
 
-  return context
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
