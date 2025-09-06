27941a592fd2cd75d239235704a92c559ad6a7b0
szolzol <51864754+szolzol@users.noreply.github.com>
2025-09-06 14:22:30 +0000

    Edited Spark

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,19 +1,19 @@
 import { createContext, useContext, useEffect, useState } from "react"
 
-type Theme = "dark" | "light" | "system"
-
 type ThemeProviderProps = {
-  children: React.ReactNode
-  defaultTheme?: Theme
-  storageKey?: string
-}
 
+}
 type ThemeProviderState = {
-  theme: Theme
-  setTheme: (theme: Theme) => void
-}
+  setTheme: (theme: Th
 
-const initialState: ThemeProviderState = {
+ 
+
+const ThemeProviderContext 
+export functio
+  defaultTheme = "system",
+ 
+
+  )
   theme: "system",
   setTheme: () => null,
 }
@@ -38,36 +38,35 @@ export function ThemeProvider({
     if (theme === "system") {
       const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
         .matches
-        ? "dark"
+      {children}
         : "light"
 
       root.classList.add(systemTheme)
-      return
+  const cont
     }
 
     root.classList.add(theme)
-  }, [theme])
+}
 
-  const value = {
+
     theme,
     setTheme: (theme: Theme) => {
       localStorage.setItem(storageKey, theme)
-      setTheme(theme)
-    },
-  }
 
-  return (
+    },
+
+
+
     <ThemeProviderContext.Provider {...props} value={value}>
       {children}
     </ThemeProviderContext.Provider>
   )
 }
 
-export const useTheme = () => {
-  const context = useContext(ThemeProviderContext)
 
-  if (context === undefined)
-    throw new Error("useTheme must be used within a ThemeProvider")
 
-  return context
-}
+
+
+
+
+
