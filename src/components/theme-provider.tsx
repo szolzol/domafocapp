e49ad181d851a7f0fcfc512ff085b1d19bf5e04e
szolzol <51864754+szolzol@users.noreply.github.com>
2025-09-06 14:12:24 +0000

    Edited Spark

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,13 +2,13 @@ import { createContext, useContext, useEffect, useState } from "react"
 
 type Theme = "dark" | "light" | "system"
 
-type ThemeProviderProps = {
+  defaultTheme?: Theme
   children: React.ReactNode
   defaultTheme?: Theme
   storageKey?: string
-}
+ 
 
-type ThemeProviderState = {
+const initialState: ThemePr
   theme: Theme
   setTheme: (theme: Theme) => void
 }
@@ -22,46 +22,46 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
 
 export function ThemeProvider({
   children,
-  defaultTheme = "system",
+
   storageKey = "vite-ui-theme",
   ...props
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(
     () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
-  )
+   
 
-  useEffect(() => {
-    const root = window.document.documentElement
 
-    root.classList.remove("light", "dark")
+  const context = useContext(ThemeProviderContex
 
-    if (theme === "system") {
-      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-        .matches
-        ? "dark"
-        : "light"
 
-      root.classList.add(systemTheme)
-      return
-    }
 
-    root.classList.add(theme)
-  }, [theme])
 
-  const value = {
-    theme,
-    setTheme: (theme: Theme) => {
-      localStorage.setItem(storageKey, theme)
-      setTheme(theme)
-    },
-  }
 
-  return (
-    <ThemeProviderContext.Provider {...props} value={value}>
-      {children}
-    </ThemeProviderContext.Provider>
-  )
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export const useTheme = () => {
   const context = useContext(ThemeProviderContext)
